8813fe540cf040d456681df9b340aceca7eeef50
Kamleswar <87739669+Kamleswar@users.noreply.github.com>
2026-07-21 14:44:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("AI Samadhan Axom");
+const API_KEY = "YAHAN_APNI_API_KEY";
+
+function addMessage(text, type) {
+  const chat = document.getElementById("chat");
+
+  const div = document.createElement("div");
+  div.className = "message " + type;
+  div.innerHTML = text;
+
+  chat.appendChild(div);
+  chat.scrollTop = chat.scrollHeight;
+}
