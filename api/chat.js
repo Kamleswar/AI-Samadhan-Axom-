81c607c0d37af60b66a5ea1fb6e69b3ca74df415
Kamleswar <87739669+Kamleswar@users.noreply.github.com>
2026-07-23 16:53:54 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       }
     );
 
-    const data = await response.json();
+    const data = await response.json();console.log(JSON.stringify(data));
 
     const reply =
       data.candidates?.[0]?.content?.parts?.[0]?.text ||
