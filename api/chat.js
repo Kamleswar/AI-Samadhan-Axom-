0c6ee0cba598606d9612fd0eb415c2c3e6a72916
Kamleswar <87739669+Kamleswar@users.noreply.github.com>
2026-07-24 16:31:06 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,7 @@ module.exports = async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent?key=${API_KEY}`,
+      `gemini-2.5-flash}`,
       {
         method: "POST",
         headers: {
